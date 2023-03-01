b93882bc66e31a295b00e28cb35294e92bc3d716
Sam <76117618+smccay@users.noreply.github.com>
2023-03-01 18:43:47 +0000

    Random encryption
    Random encryption now working returns decryption key. Decryption not implemented for new encryption.

--- SDE Visual Studios File/SDE.cpp
+++ SDE Visual Studios File/SDE.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <array>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 std::vector<std::string> input(std::string_view x)
 {
@@ -14,6 +16,28 @@ std::vector<std::string> input(std::string_view x)
 }
 std::vector<std::string> encrypt(std::vector<std::string> password)
 {
+    srand(time(NULL));
+    int shift{ 0 };
+    while (shift == 0) 
+    { 
+        shift = rand() % 11 - 5;
+    }
+    std::cout << shift <<"\n";
+    int x{ 0 };
+    int y{ 0 };
+    if (shift > 0)
+    {
+        x = shift;
+        //password.insert(password.begin(), std::to_string(x));
+    }
+    else
+    {
+        y = shift*-1;
+        //password.insert(password.begin(), std::to_string(shift));
+    }
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+
     std::array<char, 95>  characters{ ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?',
                     '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
@@ -25,17 +49,18 @@ std::vector<std::string> encrypt(std::vector<std::string> password)
     for (int i = 0; i < length; i++)
     {
          //Debug: std::cout <<"\n" << password[0][i] << ": outer loop \n";
-        for (int i1 = 0; i1 < 95; i1++)
+        for (int i1 = 0+y; i1 < 95-x; i1++)
         {
-            if (characters[i1] == password[0][i] && characters[i1] != characters[92] && characters[i1] != characters[93] && characters[i1] != characters[94])
+            if (characters[i1] == password[0][i])
             {
                 //Debug:std::cout<< characters[i1] << ": inside if statement \n";
-                password[0][i] = characters[i1 + 3];
+                password[0][i] = characters[i1 + x - y];
                 //Debug:std::cout << password[0][i] << ": Success - \n";
                 break;
             }
         }
     }
+    password.insert(password.begin(), std::to_string(shift));
     return password;
 }
 std::vector<std::string> decrypt(std::vector<std::string> userEncrypted)
@@ -51,9 +76,9 @@ std::vector<std::string> decrypt(std::vector<std::string> userEncrypted)
     for (int i = 0; i < length; i++)
     {
         //Debug: std::cout <<"\n" << password[0][i] << ": outer loop \n";
-        for (int i1 = 0; i1 < 95; i1++)
+        for (int i1 = 2; i1 < 91; i1++)
         {
-            if (characters[i1] == userEncrypted[0][i] && characters[i1] != characters[92] && characters[i1] != characters[93] && characters[i1] != characters[94] != characters[i1] != characters[0] && characters[1] != characters[2])
+            if (characters[i1] == userEncrypted[0][i])
             {
                 //Debug:std::cout<< characters[i1] << ": inside if statement \n";
                 userEncrypted[0][i] = characters[i1 - 3];
@@ -70,15 +95,14 @@ int main()
 {
     std::vector<std::string> password{ input("encrypted")};
     std::vector<std::string> encrypted{ encrypt(password) };
-    for (auto number : encrypted)
-    {
-        std::cout <<"Your password encrypted is " << number << ' ' <<"\n";
-      }
-    std::vector<std::string> enPassword{ input("decrypted") };
-    std::vector<std::string> decrypted{ decrypt(enPassword) };
-    for (auto number : decrypted)
-    {
-        std::cout << "Your password decrypted is " << number << ' ' << "\n";
+    for (const auto& str : encrypted) {
+        std::cout << str << " ";
     }
+    //std::vector<std::string> enPassword{ input("decrypted") };
+    //std::vector<std::string> decrypted{ decrypt(enPassword) };
+    //for (auto number : decrypted)
+    //{
+        //std::cout << "Your password decrypted is " << number << ' ' << "\n";
+    //}
     return 0;
 }
